8a1ce2ebe90bf99c35dcf81e9fe00227ba6bcf33
Chris Allen <chris.j.allen1979@gmail.com>
2026-02-04 12:48:19 +0000

    Automated conversion from geerlingguy.nodejs Ansible role

--- manifests/init.pp
+++ manifests/init.pp
@@ -42,33 +42,33 @@ class paw_ansible_role_nodejs (
 # Execute the Ansible role using PAR (Puppet Ansible Runner)
 # Playbook synced via pluginsync to agent's cache directory
 # Check for common paw::par_vardir setting, then module-specific, then default
-  $_par_vardir = $par_vardir ? {
-    undef   => lookup('paw::par_vardir', Stdlib::Absolutepath, 'first', '/opt/puppetlabs/puppet/cache'),
-    default => $par_vardir,
-  }
-  $playbook_path = "${_par_vardir}/lib/puppet_x/ansible_modules/ansible_role_nodejs/playbook.yml"
+$_par_vardir = $par_vardir ? {
+  undef   => lookup('paw::par_vardir', Stdlib::Absolutepath, 'first', '/opt/puppetlabs/puppet/cache'),
+  default => $par_vardir,
+}
+$playbook_path = "${_par_vardir}/lib/puppet_x/ansible_modules/ansible_role_nodejs/playbook.yml"
 
-  par { 'paw_ansible_role_nodejs-main':
-    ensure        => present,
-    playbook      => $playbook_path,
-    playbook_vars => {
-      'npm_config_prefix'           => $npm_config_prefix,
-      'nodejs_version'              => $nodejs_version,
-      'npm_config_unsafe_perm'      => $npm_config_unsafe_perm,
-      'nodejs_npm_global_packages'  => $nodejs_npm_global_packages,
-      'nodejs_package_json_path'    => $nodejs_package_json_path,
-      'nodejs_generate_etc_profile' => $nodejs_generate_etc_profile,
-    },
-    tags          => $par_tags,
-    skip_tags     => $par_skip_tags,
-    start_at_task => $par_start_at_task,
-    limit         => $par_limit,
-    verbose       => $par_verbose,
-    check_mode    => $par_check_mode,
-    timeout       => $par_timeout,
-    user          => $par_user,
-    env_vars      => $par_env_vars,
-    logoutput     => $par_logoutput,
-    exclusive     => $par_exclusive,
-  }
+par { 'paw_ansible_role_nodejs-main':
+  ensure        => present,
+  playbook      => $playbook_path,
+  playbook_vars => {
+        'npm_config_prefix' => $npm_config_prefix,
+        'nodejs_version' => $nodejs_version,
+        'npm_config_unsafe_perm' => $npm_config_unsafe_perm,
+        'nodejs_npm_global_packages' => $nodejs_npm_global_packages,
+        'nodejs_package_json_path' => $nodejs_package_json_path,
+        'nodejs_generate_etc_profile' => $nodejs_generate_etc_profile
+              },
+  tags          => $par_tags,
+  skip_tags     => $par_skip_tags,
+  start_at_task => $par_start_at_task,
+  limit         => $par_limit,
+  verbose       => $par_verbose,
+  check_mode    => $par_check_mode,
+  timeout       => $par_timeout,
+  user          => $par_user,
+  env_vars      => $par_env_vars,
+  logoutput     => $par_logoutput,
+  exclusive     => $par_exclusive,
+}
 }
